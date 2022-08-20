be3adbb47126e517b41ba4543ef0cd2f5035c907
Lawrence Splaver <lsplaver263@gmail.com>
2022-08-20 17:14:47 -0400

    Finished the code for version 1.  Disabled TotalSilver and TotalCopper for the moment.  Customizable length of year for game setting.

--- FantasyRPGFutureValue/Form1.cs
+++ FantasyRPGFutureValue/Form1.cs
@@ -24,7 +24,7 @@ namespace FantasyRPGFutureValue
 
                     monthlyInvestment = CalculateFutureValue(monthlyInvestment, interestRate, years, monthsPerYear);
 
-
+                    txtValueGold.Text = monthlyInvestment.ToString("C");
                 }
             }
             catch(Exception ex)
@@ -39,9 +39,9 @@ namespace FantasyRPGFutureValue
             decimal monthlyInterestRate = ((interestRate / monthsPerYear) / 100);
             int months = monthsPerYear * years;
 
-            for (int x = 0; x < months; months++)
+            for (int x = 0; x < months; x++)
             {
-                result += monthlyInvestment * (1 + monthlyInterestRate);
+                result = (result + monthlyInvestment) * (1 + monthlyInterestRate);
             }
 
             return result;
@@ -50,7 +50,9 @@ namespace FantasyRPGFutureValue
         private decimal MonthlyInvestmentDecimal(long monthlyInvestmentGold, long monthlyInvestmentSilver, long monthlyInvestmentCopper)
         {
             decimal monthlyInvestment = 0;
-            monthlyInvestment = (((monthlyInvestmentGold * 100) + (monthlyInvestmentSilver * 10) + monthlyInvestmentCopper) / 100);
+
+            monthlyInvestment = ((monthlyInvestmentCopper / 100) + (monthlyInvestmentSilver / 10)) + monthlyInvestmentGold;
+
             return monthlyInvestment;
         }
 
@@ -59,7 +61,40 @@ namespace FantasyRPGFutureValue
             bool success = true;
             string errorMessage = "";
 
+            if (txtMonthlyGold.Text == "" && txtMonthlySilver.Text == "" && txtMonthlyCopper.Text == "")
+            {
+                errorMessage += "Monthly Gold, Monthly Silver and Monthly Copper can't be all null.\n";
+            }
+            if (txtMonthlyGold.Text == "")
+            {
+                txtMonthlyGold.Text = "0";
+            }
+            if (txtMonthlySilver.Text == "")
+            {
+                txtMonthlySilver.Text = "0";
+            }
+            if (txtMonthlyCopper.Text == "")
+            {
+                txtMonthlyCopper.Text = "0";
+            }
+
             errorMessage += IsDecimal(txtMonthlyGold.Text, txtMonthlyGold.Tag.ToString());
+            errorMessage += IsWithinRange(txtMonthlyGold.Text, txtMonthlyGold.Tag.ToString(), 0, 100000);
+
+            errorMessage += IsDecimal(txtMonthlySilver.Text, txtMonthlySilver.Tag.ToString());
+            errorMessage += IsWithinRange(txtMonthlySilver.Text, txtMonthlySilver.Tag.ToString(), 0, 100000);
+
+            errorMessage += IsDecimal(txtMonthlyCopper.Text, txtMonthlyCopper.Tag.ToString());
+            errorMessage += IsWithinRange(txtMonthlyCopper.Text, txtMonthlyCopper.Tag.ToString(), 0, 100000);
+
+            errorMessage += IsDecimal(txtInterestRate.Text, txtInterestRate.Tag.ToString());
+            errorMessage += IsWithinRange(txtInterestRate.Text, txtInterestRate.Tag.ToString(), 1, 20);
+
+            errorMessage += IsInt32(txtYears.Text, txtYears.Tag.ToString());
+            errorMessage += IsWithinRange(txtYears.Text, txtYears.Tag.ToString(), 1, 100);
+
+            errorMessage += IsInt32(txtMonths.Text, txtMonths.Tag.ToString());
+            errorMessage += IsWithinRange(txtMonths.Text, txtMonths.Tag.ToString(), 1, 20);
 
             if (errorMessage != "")
             {
@@ -69,9 +104,48 @@ namespace FantasyRPGFutureValue
             return success;
         }
 
-        private string IsDecimal(string text, string? v)
+        private string IsInt32(string value, string name)
         {
-            throw new NotImplementedException();
+            string msg = "";
+            
+            if (!Int32.TryParse(value, out _))
+            {
+                msg += name + " must be a valid integer value.\n";
+            }
+
+            return msg;
+        }
+
+        private string IsWithinRange(string value, string name, decimal min, decimal max)
+        {
+            string msg = "";
+
+            if (Decimal.TryParse(value, out decimal number))
+            {
+                if (number < min || number > max)
+                {
+                    msg += name + " must be between " + min + " and " + max + ".\n";
+                }
+            }
+
+            return msg;
+        }
+
+        private string IsDecimal(string value, string name)
+        {
+            string msg = "";
+
+            if (!Decimal.TryParse(value, out _))
+            {
+                msg += name + " must be a valid decimal value.\n";
+            }
+
+            return msg;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
